312063858eed6edea037fdd273d6ce00d51e34c6
thomasbititude <thomas@bititude.com>
2022-01-12 10:55:07 +0530

    sample validator added in post request

--- controllers/shoppingController.js
+++ controllers/shoppingController.js
@@ -1,43 +1,56 @@
-let Item = require('../models/shoppingmodel');
+let Item = require("../models/shoppingmodel");
+const { validationResult } = require("express-validator");
 
+exports.items = async (req, res) => {
+  try {
+    let items = await Item.fetchAll();
+    res.json(items);
+  } catch (error) {
+    res.send(error);
+  }
+};
 
-exports.items =  async (req, res) => {
-    try{let items = await Item.fetchAll();
-        res.json(items);
-    } catch (error){
-        res.send(error);
+exports.items_id = async (req, res) => {
+  try {
+    let items_id = await Item.where("id", parseInt(req.params.id)).fetch();
+    res.json(items_id);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+exports.items_post = async (req, res) => {
+    const errors = validationResult(req);
+  try {const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send('invalid item.Try again');
     }
-    };
+    let items = await Item.forge({ ...req.body }).save();
+    res.json(items);
+  } catch (error) {
+    res.send(error);
+  }
+};
 
-exports.items_id = async(req,res) => {
-   try{let items_id = await Item.where('id',parseInt(req.params.id)).fetch();
-   res.json(items_id);
+exports.items_put = async (req, res) => {
+  try {
+    let items = await Item.where("id", parseInt(req.params.id)).save(
+      { ...req.body },
+      { patch: true }
+    );
+    res.json(items);
+  } catch (error) {
+    res.send(error);
+  }
+};
 
-   } catch (error){
-       res.send(error);
-   }
-   };
-
-exports.items_post = async(req,res) => {
-    try{let items = await Item.forge({...req.body}).save();
-        res.json(items);
-    }catch (error){
-        res.send(error);
-    }
-    };
-
-exports.items_put = async(req,res) => {
-    try{let items = await Item.where('id',parseInt(req.params.id)).save({...req.body},{patch:true});
-        res.json(items);
-    }catch (error){
-        res.send(error);
-    }
-    }; 
- 
-exports.items_delete = async(req,res) => {
-    try{let items = await Item.where('id',parseInt(req.params.id)).destroy({...req.body});
-        res.json(items);
-    }catch (error){
-        res.send(error);
-    }
-    }; 
+exports.items_delete = async (req, res) => {
+  try {
+    let items = await Item.where("id", parseInt(req.params.id)).destroy({
+      ...req.body,
+    });
+    res.json(items);
+  } catch (error) {
+    res.send(error);
+  }
+};
--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,12 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let shopping_controller = require('../controllers/shoppingController');
+let shopping_controller = require("../controllers/shoppingController");
+const {body} = require("express-validator");
 
-
-router.get('/', shopping_controller.items);
-router.get('/:id',shopping_controller.items_id);
-router.post('/',shopping_controller.items_post);
-router.put('/:id',shopping_controller.items_put);
-router.delete('/:id',shopping_controller.items_delete);
+router.get("/", shopping_controller.items);
+router.get("/:id", shopping_controller.items_id);
+router.post("/",body('item').isLength({min:3}), shopping_controller.items_post);
+router.put("/:id", shopping_controller.items_put);
+router.delete("/:id", shopping_controller.items_delete);
 
 module.exports = router;
